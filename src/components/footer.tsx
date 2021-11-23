a9fb8bff13d2eb9211cba182b9d300f28f66dfef
chhum chanleak <chhum.chanleak1994@gmail.com>
2021-11-23 18:45:02 +0100

    added SocialContact component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SocialContact from "./common/social-contact";
 
 function Footer() {
   return (
     <div className="footer" style={{ marginTop: "40px" }}>
-      Have Fun !
+      <SocialContact />
     </div>
   );
 }
